f58562b77fc74f51f655c773ed33c362804c0451
ayham <ayham19al19mazari@gmail.com>
2021-03-10 02:45:47 +0200

    Complete

---
+++ app/Http/Middleware/CheckIfLoggedIn.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckIfLoggedIn
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(!Auth::check()){
+            return response()->json(["User Must Be Logged In"]);
+        }
+        return $next($request);
+    }
+}
