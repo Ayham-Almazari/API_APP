5de352f3f5406b637b6b07d617bb7bac3aebb69c
ayham <ayham19al19mazari@gmail.com>
2021-04-16 17:43:45 +0300

    aaa

--- app/Http/Controllers/API/OwnerControllers/FactoryCategories.php
+++ app/Http/Controllers/API/OwnerControllers/FactoryCategories.php
@@ -23,7 +23,6 @@ class FactoryCategories extends Controller
     public function __construct()
     {
         $this->middleware(['auth:owner']);
-        Auth::shouldUse('owner');
         $this->owner = auth()->user();
     }
 
